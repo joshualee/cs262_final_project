bb33a6195ce63a3e4e340dab51f36ca5f93a23f6
hcanderson <hcanderson2013@gmail.com>
2014-04-25 13:51:02 -0400

    changes to make it compile + console

--- src/edu/harvard/cs262/crypto/CentralServer.java
+++ src/edu/harvard/cs262/crypto/CentralServer.java
@@ -80,13 +80,15 @@ public class CentralServer implements CryptoServer {
 		assertClientRegistered(victim);
 		
 		// TODO: assumes if victim is a client, then vicList won't be null
-		List<String> vicList = notifications.get(victim);
-		
-		if (!vicList.contains(listener)) {
-			vicList.add(listener);
-		}
-		else {
-			System.out.println(String.format("Warning: %s is already listening to %s", listener, victim));
+		if (notifications.containsKey(victim)){
+			List<String> vicList = notifications.get(victim);
+			
+			if (!vicList.contains(listener)) {
+				vicList.add(listener);
+			}
+			else {
+				System.out.println(String.format("Warning: %s is already listening to %s", listener, victim));
+			}
 		}
 	}
 	
