7aabc4681e64949acb70174a1c8d584bbe951e65
hcanderson <hcanderson2013@gmail.com>
2014-05-05 18:04:36 -0400

    update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 java -cp "bin/:lib/*" org.junit.runner.JUnitCore edu.harvard.cs262.tests.ClientServerTests
 java -cp "bin/:lib/*" org.junit.runner.JUnitCore edu.harvard.cs262.tests.CryptoCipherTests
 java -cp "bin/:lib/*" org.junit.runner.JUnitCore edu.harvard.cs262.tests.EVoteTests
-java -classpath bin edu.harvard.cs262.tests.ConsoleTest
+java -cp bin edu.harvard.cs262.tests.ConsoleTest
