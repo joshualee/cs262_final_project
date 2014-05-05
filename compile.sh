9e72427926d0a5f96b5e9c5a0f79050d1507e1ec
hcanderson <hcanderson2013@gmail.com>
2014-05-05 18:57:48 -0400

    Delete compile.sh

--- compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-(cd src && javac -g -d ../bin edu/harvard/cs262/crypto/*.java)
-(cd src && javac -g -d ../bin edu/harvard/cs262/crypto/*/*.java)
-(cd tests && javac -g -d ../bin -cp "../bin:../lib/junit.jar" edu/harvard/cs262/tests/*.java)
-(cd bin && rmic edu.harvard.cs262.crypto.server.CentralServer)
